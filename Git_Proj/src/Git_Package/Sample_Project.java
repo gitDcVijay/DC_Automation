63975480c8449a6e77262aea4fa803a153a29649
Vijayanageswaran <vijayanageswaran.g@cognizant.com>
2016-02-09 03:07:56 -0500

    New code added

---
+++ Git_Proj/src/Git_Package/Sample_Project.java
@@ -0,0 +1,14 @@
+package Git_Package;
+
+public class Sample_Project {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("This is a sample project");
+		System.out.println("This is a sample project to verify the Git Hub integration with Eclipse");
+		
+
+	}
+
+}
