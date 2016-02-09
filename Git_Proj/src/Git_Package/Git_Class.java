7c7fcb43ffac7fb52ed51ca41e8989a20f12bae8
Vijayanageswaran <vijayanageswaran.g@cognizant.com>
2016-02-09 01:57:12 -0500

    Initial Check-in

---
+++ Git_Proj/src/Git_Package/Git_Class.java
@@ -0,0 +1,12 @@
+package Git_Package;
+
+public class Git_Class {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("This is a sample Git Project");
+
+	}
+
+}
